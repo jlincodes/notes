99bc5d56a818272d3592f071e6ed6f17e0ff9b66
Julie <julie.lin0812@gmail.com>
2018-03-12 13:26:50 -0700

    Adds alternative solution

--- algorithm_problems/generate_pw.js
+++ algorithm_problems/generate_pw.js
@@ -8,14 +8,34 @@ function generatePassword(length) {
   const characters = [uppercaseLetters, lowercaseLetters, numbers];
   const results = [];
 
-  while (results.length < length) {
+  // Using a while loop
+  // drawback of using a while loop: infinite loop if one omitted results.push(char)
+
+  // while (results.length < length) {
+  //   const key = getRandomNum(characters.length);
+  //   const charList = characters[key];
+  //   const char = charList[getRandomNum(charList.length)];
+  //   results.push(char);
+  // }
+
+  // Using a for loop
+
+  for (let i = 0; i < length; i++) {
     const key = getRandomNum(characters.length);
     const charList = characters[key];
-    const char = characters[getRandomNum(charList.length)];
+    const char = charList[getRandomNum(charList.length)];
     results.push(char);
   }
 
   return results.join('');
+
+  // using Array.prototype.map
+
+  // return new Array(length).fill(null).map(() => {
+  //   const key = getRandomNum(characters.length);
+  //   const charList = characters[key];
+  //   return charList[getRandomNum(charList.length)];
+  // }).join('');
 }
 
 function getRandomNum(num) {
