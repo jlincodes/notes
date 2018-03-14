d352f182c99e66b87e33fb43a91433d3bfc25fc3
Julie <julie.lin0812@gmail.com>
2018-03-14 11:01:05 -0700

    Refactors solution

--- algorithm_problems/str_str.js
+++ algorithm_problems/str_str.js
@@ -1,12 +1,15 @@
 const strStr = (haystack, needle) => {
+    if (haystack.length === 0 && needle.length === 0) return 0;
+
     const nLength = needle.length;
 
-    for (let i = 0; i < haystack.length; i ++) {
+    for (let i = 0; i <= haystack.length - nLength; i ++) {
         const slice = haystack.slice(i,  i + nLength);
-        if (slice === needle) return true;
+        if (slice === needle) return i;
     }
-    return false;
+    return -1;
 };
 
+console.log(strStr("", ""));
 console.log(strStr("hello world", "ll"));
 console.log(strStr("hello world", "ld"));
