c5da107be6f0d24cb8df3f78a31bfff12bf57d44
Julie <julie.lin0812@gmail.com>
2018-03-14 11:12:34 -0700

    Adds group anagrams problem

---
+++ algorithm_problems/group_anagrams.js
@@ -0,0 +1,8 @@
+// given an array of strings, return the strings grouped by anagrams.
+// ['cat', 'dog', 'act', 'tac', 'god', 'cars', 'scar', 'foobar']
+// => [
+//   ['cat', 'act', 'tac'],
+//   ['dog', 'god'],
+//   ['cars', 'scar'],
+//   ['foobar']
+// ]
