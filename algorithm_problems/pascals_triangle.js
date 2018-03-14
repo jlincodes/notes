c5f5f11ebbfbe99760a8e34db5348bc9e1609110
Julie Lin <julie.lin0812@gmail.com>
2018-03-13 22:49:41 -0700

    Adds solution

--- algorithm_problems/pascals_triangle.js
+++ algorithm_problems/pascals_triangle.js
@@ -10,3 +10,27 @@
 //   [1,3,3,1],
 //  [1,4,6,4,1]
 // ]
+
+const generate = (numRows) => {
+    const arr = [];
+
+    if (numRows === 0) return arr;
+
+    arr.push([1]);
+
+    for (let i = 1; i < numRows; i++) {
+        const newArr = [];
+        const prevArr = arr[i-1];
+
+        for (let j = 0; j <= prevArr.length; j++) {
+            const prev = prevArr[j - 1] ? prevArr[j - 1] : 0;
+            const curr = prevArr[j] ? prevArr[j] : 0;
+            newArr.push(prev + curr);
+        }
+
+        arr.push(newArr);
+
+    }
+
+    return arr;
+};
