b1f274ebb3b816480707d4d93a667c80fd691e4c
Julie Lin <julie.lin0812@gmail.com>
2018-03-13 22:57:18 -0700

    Add solution

--- algorithm_problems/convert_time.js
+++ algorithm_problems/convert_time.js
@@ -6,3 +6,15 @@ Example:
 "07:45:20PM" => "19:45:20"
 
 */
+
+const convertTime = (s) => {
+	const result = s.split('');
+  if (result[result.length - 2].toUpperCase() === "P") {
+  	let hour = parseInt(result.slice(0,2).join(''));
+    hour += 12;
+    hour = hour.toString().split('');
+    [result[0], result[1]] = [hour[0], hour[1]]
+  }
+  result.splice(result.length - 2, 2)
+  return result.join('');
+}
