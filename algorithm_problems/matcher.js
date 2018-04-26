ab76635744dc4a7bd83c1b2dddf3d70c18d6de8e
Julie <julie.lin0812@gmail.com>
2018-04-25 19:28:20 -0700

    Add solution

--- algorithm_problems/matcher.js
+++ algorithm_problems/matcher.js
@@ -1,4 +1,29 @@
 function nameMatcher(knownAliases, name) {
   // some code here
   const nameArr = name.split(' ');
+  // iterate over each alias
+  for (let i = 0; i < knownAliases.length; i++) {
+    const alias = knownAliases[i];
+    const aliasArr = alias.split(' ');
+    if (nameArr.length === 2 || aliasArr.length === 2) {
+      return (isMatch(aliasArr[0], nameArr[0])
+        && isMatch(aliasArr[aliasArr.length - 1], nameArr[nameArr.length -1]));
+    } else if (nameArr.length === 3 && aliasArr.length === 3) {
+        if (isMatch(alias, name)) return true;
+    }
+  }
+  return false;
+}
+
+function isMatch(alias, name) {
+  return (new RegExp(name, 'i')).test(alias);
+}
+
+function assertEqual(expected, result, errorMsg) {
+  if (result !== expected) {
+    console.log(errorMsg);
+    console.log(`expect: ${expected}`);
+    console.log(`actual: ${result}`);
+    console.log('');
+  }
 }
