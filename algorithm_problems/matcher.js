36b463d446a5618ede19b3eac5bd4ee54f554955
Julie <julie.lin0812@gmail.com>
2018-04-25 19:57:12 -0700

    Update solution to handle edge case of middle initials

--- algorithm_problems/matcher.js
+++ algorithm_problems/matcher.js
@@ -1,22 +1,35 @@
 function nameMatcher(knownAliases, name) {
-  // some code here
   const nameArr = name.split(' ');
-  // iterate over each alias
+
   for (let i = 0; i < knownAliases.length; i++) {
+
     const alias = knownAliases[i];
     const aliasArr = alias.split(' ');
+
     if (nameArr.length === 2 || aliasArr.length === 2) {
       return (isMatch(aliasArr[0], nameArr[0])
         && isMatch(aliasArr[aliasArr.length - 1], nameArr[nameArr.length -1]));
     } else if (nameArr.length === 3 && aliasArr.length === 3) {
-        if (isMatch(alias, name)) return true;
+      if (isMatch(alias, name)) return true;
+      if (isMatch(aliasArr[0], nameArr[0]) &&
+          isMatch(aliasArr[aliasArr.length - 1], nameArr[nameArr.length -1]) &&
+          checkMiddle(aliasArr[1], nameArr[1])) {
+          return true;
+      }
     }
   }
+
   return false;
 }
 
 function isMatch(alias, name) {
   return (new RegExp(name, 'i')).test(alias);
+}
+
+function checkMiddle(aliasMid, nameMid) {
+  if (aliasMid.length === 1 || nameMid.length === 1) {
+    return isMatch(aliasMid.slice(0,1), nameMid.slice(0,1));
+  }
 }
 
 function assertEqual(expected, result, errorMsg) {
