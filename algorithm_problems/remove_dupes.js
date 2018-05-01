8b59e31577ee863a779337e0d0401c52b5a14b22
Julie <julie.lin0812@gmail.com>
2018-04-30 21:35:50 -0700

    Adds solution

--- algorithm_problems/remove_dupes.js
+++ algorithm_problems/remove_dupes.js
@@ -12,3 +12,20 @@
 //
 // Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
 // It doesn't matter what values are set beyond the returned length.
+
+const removeDuplicates = (nums) => {
+  let counter = 1;
+  for (let i = 1; i < nums.length; i++) {
+    const prev = nums[i - 1];
+    const curr = nums[i];
+    if (prev !== curr) {
+      nums[counter] = nums[i];
+      counter ++;
+    }
+  }
+  // console.log(nums);
+  return counter;
+};
+
+console.log(removeDuplicates([1,1,2]));
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
