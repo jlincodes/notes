a34fd4bf4aad012e33aa3ec0170750789e090c55
Julie <julie.lin0812@gmail.com>
2018-04-30 21:56:48 -0700

    Add solution

--- algorithm_problems/remove_dupes_II.js
+++ algorithm_problems/remove_dupes_II.js
@@ -16,3 +16,23 @@
 // Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
 //
 // It doesn't matter what values are set beyond the returned length.
+
+const removeDuplicates = (nums) => {
+  let counter = 2;
+  for (let i = 2; i < nums.length; i++) {
+    const prevPrev = nums[counter - 2];
+    const prev = nums[counter - 1];
+    const curr = nums[i];
+    if ((prev !== curr && prevPrev !== curr) || (prev === curr && prevPrev !== curr)) {
+      nums[counter] = nums[i];
+      counter ++;
+    } else if (prev !== curr && prevPrev === curr) {
+      counter ++;
+    }
+  }
+  // console.log(nums);
+  return counter;
+};
+
+console.log(removeDuplicates([1,1,1,2,2,3]) === 5);
+console.log(removeDuplicates([0,0,1,1,1,1,2,3,3]) === 7);
