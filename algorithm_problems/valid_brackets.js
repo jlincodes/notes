bb604a85212293e2693dac6e06ccd43e115e141d
Julie <julie.lin0812@gmail.com>
2018-04-30 20:56:13 -0700

    Implement valid brackets solution using a stack

--- algorithm_problems/valid_brackets.js
+++ algorithm_problems/valid_brackets.js
@@ -26,3 +26,54 @@
 //
 // Input: "{[]}"
 // Output: true
+
+class myStack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(el) {
+    this.stack.push(el);
+    // returning `this` to return the stack object instead of the stack array
+    // which will prevent the stack order from being modified
+    return this;
+  }
+
+  pop() {
+    this.stack.pop();
+  }
+
+  // looks at the last element that was added to the stack
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  size() {
+    return this.stack.length;
+  }
+}
+
+const isValid = s => {
+  if (s.length % 2 === 1) return false;
+
+  const pairs = {
+    "(":")",
+    "[":"]",
+    "{":"}"
+  };
+  const newStack = new myStack();
+
+  for (let i = 0; i < s.length; i++) {
+    const temp = s[i];
+    if (pairs.hasOwnProperty(temp)) {
+      newStack.push(temp);
+    } else {
+      if (temp !== pairs[newStack.peek()]) {
+        return false;
+      } else {
+        newStack.pop();
+      }
+    }
+  }
+  return newStack.size() === 0 ? true : false;
+};
